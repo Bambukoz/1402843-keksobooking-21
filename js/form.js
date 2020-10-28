4a78ba5db712d7407430ada6ae2bcc9369d059a2
Ezhov Alexey <ezhov-a@yandex.ru>
2020-10-28 15:21:38 +0300

    Все, начинаю делать глупые ошибки, пора остановиться

--- js/form.js
+++ js/form.js
@@ -30,12 +30,12 @@ const onCapacityChange = () => {
 };
 
 const onTimeChange = (evt) => {
-  const target = evt.target;
-  target = (evt.target === form.timein) ?
-    form.timeout.value = form.timein.value :
+  if (evt.target === form.timein) {
+    form.timeout.value = form.timein.value;
+  } else {
     form.timein.value = form.timeout.value;
+  }
 };
-
 
 const onFormElementChange = (evt) => {
   switch (evt.target) {
